e0ae8a3ff51491cb50d2d7a564db047426d568c7
MechaKnightz <MechaKnightz@hotmail.com>
2021-01-07 04:09:02 +0100

    need to commit more

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,12 @@
 use speedrun;
 /* replace this with DB_DATABASE environmental variable, with script maybe? */
-CREATE TABLE speedruns (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `time` INT NOT NULL,
-  `description` TEXT NULL,
-  `url` VARCHAR(2083) NOT NULL COMMENT 'url to video of run',
-  `type` ENUM(
+CREATE TABLE `Speedruns` (
+  `SpeedrunID` INT UNIQUE NOT NULL AUTO_INCREMENT,
+  `Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Time` INT NOT NULL,
+  `Description` TEXT NULL,
+  `Url` VARCHAR(2083) NOT NULL COMMENT 'url to video of run',
+  `Type` ENUM(
     'ANY_PERCENT',
     'FULL_GAME_GLITCHLESS',
     'HUNDRED_PERCENT',
@@ -15,65 +15,30 @@ CREATE TABLE speedruns (
     'DEATH_PERCENT',
     'NO_BOAT'
   ) NOT NULL,
-  PRIMARY KEY (`id`)
+  `SubmitterID` INT NOT NULL REFERENCES `Users` (`UserID`),
+  `VerifierID` INT NULL REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`SpeedrunID`, `SubmitterID`)
 ) ENGINE = InnoDB;
-INSERT INTO `speedruns` (
-    `time`,
-    `description`,
-    `url`,
-    `type`
-  )
-VALUES (
-    '42131',
-    'best run ever',
-    'youtube.com',
-    'ANY_PERCENT'
-  );
-INSERT INTO `speedruns` (
-    `time`,
-    `description`,
-    `url`,
-    `type`
-  )
-VALUES (
-    '51232',
-    'best run ever',
-    'youtube.com',
-    'ANY_PERCENT'
-  );
-INSERT INTO `speedruns` (
-    `time`,
-    `description`,
-    `url`,
-    `type`
-  )
-VALUES (
-    '123',
-    'best run ever',
-    'youtube.com',
-    'VM_PERCENT'
-  );
-INSERT INTO `speedruns` (
-    `time`,
-    `description`,
-    `url`,
-    `type`
-  )
-VALUES (
-    '51232',
-    'best run ever',
-    'youtube.com',
-    'VM_PERCENT'
-  );
-INSERT INTO `speedruns` (
-    `time`,
-    `description`,
-    `url`,
-    `type`
-  )
-VALUES (
-    '21341',
-    'best run ever',
-    'youtube.com',
-    'FULL_GAME_GLITCHLESS'
-  );
+
+CREATE TABLE `Users` (
+  `UserID` INT NOT NULL UNIQUE,
+  `Email` VARCHAR(254) UNIQUE NOT NULL,
+  `Name` VARCHAR(15) UNIQUE,
+  `Description` TEXT NULL,
+  `Avatar` VARCHAR(2083) NULL,
+  PRIMARY KEY (`UserID`, `Email`, `Name`)
+) ENGINE = InnoDB;
+
+/*Many to many between users if needed*/
+/* CREATE TABLE `UsersSpeedruns` (
+  `UserID` INT NOT NULL,
+  `SpeedrunID` INT NOT NULL,
+  PRIMARY KEY (`UserID`, `SpeedrunID`),
+  CONSTRAINT `Constr_UsersSpeedruns_User_fk` 
+    FOREIGN KEY `User_fk` (`User`) REFERENCES `Users` (`UserID`) 
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Constr_UsersSpeedruns_Speedrun_fk` 
+    FOREIGN KEY `Speedrun_fk` (`Speedrun`) REFERENCES `Speedruns` (`SpeedrunID`) 
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB; */
+
